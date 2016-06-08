8e7f85752b27b8d6a24e4ef8c4a964f694044cad
Cheryl Wang <cheryl.wang001@gmail.com>
2016-06-08 04:42:07 -0400

    plot4.R
    Thank you for your time:)

---
+++ plot4.R
@@ -0,0 +1,18 @@
+###project1
+##plot4
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data$Date <- as.Date(as.character(data$Date), "%d/%m/%Y")
+sub <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
+sub["DateTime"] <- paste(sub$Date, sub$Time)
+sub$DateTime <- strptime(sub$DateTime, "%Y-%m-%d %H:%M:%S")
+png(file = "plot4.png", width = 480, height = 480)
+par(mfrow = c(2, 2))
+with(sub, plot(DateTime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power"))
+with(sub, plot(DateTime, Voltage, type = "l", xlab = "datetime", ylab = "Voltage"))
+with(sub, {plot(DateTime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+  lines(DateTime, Sub_metering_2, type = "l", col = "red")
+  lines(DateTime, Sub_metering_3, type = "l", col = "blue")
+})
+legend("topright", pch = "_", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+with(sub, plot(DateTime, Global_reactive_power, type = "l", xlab = "datetime"))
+dev.off()
